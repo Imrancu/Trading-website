b2bf0842a0e5460d7c0700d0aee16eb4b6bc917c
imrancu <mdimran3772@gmail.com>
2022-02-22 10:11:03 +0600

    first commit

---
+++ app.js
@@ -0,0 +1,6 @@
+const depositBtn = document.getElementById('deposit-btn');
+const buyBtc = document.getElementById('buy-btc');
+const buyEth = document.getElementById('buy-eth');
+const balance = document.getElementById('balance');
+const bitcoinBalance = document.getElementById('bitcoin-balance');
+const ethereumBalance = document.getElementById('bitcoin-balance')
